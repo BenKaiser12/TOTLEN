c7af647ad62f29cec7d164b44190266978199b4e
Ben Kaiser <ben.kaiser12@gmail.com>
2017-03-03 13:23:54 -0700

    Re-arranged and consolidated code.

--- TOTLEN/TotalLength.cs
+++ TOTLEN/TotalLength.cs
@@ -29,38 +29,62 @@ namespace TOTLEN
             PromptSelectionResult acSelPrompt;
             acSelPrompt = ed.GetSelection();
 
-            //Check if Pickfirst set was valid
+            //Variables...
+           double totalPolyLength = 0,
+                totalLineLength = 0,
+                totalArcLength = 0,
+                totalAllLength = 0;
+
+
+            //Check if Selection is Valid
             try
             {
                 if (acSelPrompt.Status == PromptStatus.OK)
                 {
-                    //If it was valid, run the Sum methods below
                     SelectionSet acSelSet = acSelPrompt.Value;
-
-                    double totalLengthPoly,
-                        totalLengthLine,
-                        totalLengthArc;
-
-                    //Get the type of the selected object and convert it to a string
-                    string sObjType = acSelSet.GetType().ToString();
-
-                    //Check the object type, perform sum for given type
-                    if (sObjType == "Polyline")
+                    
+                    //If it was valid, run the Sum methods below
+                    using (Transaction acTrans = acDb.TransactionManager.StartTransaction())
                     {
-                        totalLengthPoly = SumEntityLength_Polyline(acSelSet);
-                    }
-                    else if (sObjType == "Line")
-                    {
-                        totalLengthLine = SumEntityLength_Line(acSelSet);
-                    }
-                    else if (sObjType == "Arc")
-                    {
-                        totalLengthArc = SumEntityLength_Arc(acSelSet);
-                    }
+                        try
+                        {
+                            //Step through Objects in Selection
+                            foreach (SelectedObject acSelObj in acSelSet)
+                            {
+                                Type acSelObjType = acSelObj.GetType();
+                                
+                                //If there is a valid object, check its type and depending
+                                //on that, add it's length to the current total
+                                if (acSelObjType == typeof(Polyline))
+                                {
+                                    Entity acEnt = acTrans.GetObject(acSelObj.ObjectId, OpenMode.ForRead) as Entity;
+                                    totalPolyLength += (acEnt as Polyline).Length;
+                                    ed.WriteMessage("Selected Polyline");
+                                }
+                                else if (acSelObjType == typeof(Line))
+                                {
+                                    Entity acEnt = acTrans.GetObject(acSelObj.ObjectId, OpenMode.ForRead) as Entity;
+                                    totalLineLength += (acEnt as Line).Length;
+                                    ed.WriteMessage("Selected Line");
+                                }
+                                else if (acSelObjType == typeof(Arc))
+                                {
+                                    Entity acEnt = acTrans.GetObject(acSelObj.ObjectId, OpenMode.ForRead) as Entity;
+                                    totalArcLength += (acEnt as Arc).Length;
+                                    ed.WriteMessage("Selected Arc");
+                                }
+                            }
+                        }
+                        catch
+                        {
+                            ed.WriteMessage("Sorry, there was an error.");
+                        }
 
-                    double TotalLength = totalLengthLine + totalLengthArc + totalLengthPoly;
+                        totalAllLength = totalArcLength + totalLineLength + totalPolyLength;                    
 
-                    string sTotalLength = TotalLength.ToString();
+                    }
+                    
+                    string sTotalLength = totalAllLength.ToString();
                     String.Format("{0:0.00}", sTotalLength);
                     ed.WriteMessage("Total Length = {0}", sTotalLength);
                 }
@@ -72,125 +96,7 @@ namespace TOTLEN
             catch
             {
                 ed.WriteMessage("I didn't do what I was supposed to");
-            }
-        
-            
-        }
-
-        private double SumEntityLength_Polyline(SelectionSet acSelSet)
-        {
-            //get Document and Databse
-            Document acDoc = Application.DocumentManager.MdiActiveDocument;
-            Database acDb = acDoc.Database;
-
-            //define editor variable
-            Editor ed = acDoc.Editor;
-
-            //Set Total Length Variable before beginning the sumation of selected lengths
-            Double totalPolyLength = 0;
-
-            using (Transaction acTrans = acDb.TransactionManager.StartTransaction())
-            {
-                try
-                {
-                    //Step through Objects in Selection
-                    foreach (SelectedObject acSelObj in acSelSet)
-                    {
-                        //If there is a valid object, add it's length to the current total
-                        if (acSelObj != null)
-                        {
-                            Entity acEnt = acTrans.GetObject(acSelObj.ObjectId, OpenMode.ForRead) as Entity;
-                            totalPolyLength += (acEnt as Polyline).Length;
-                            
-                        }
-                        
-                    }
-                }
-                catch
-                {
-                    ed.WriteMessage("Sorry, there was an error.");
-                }
-
-                return totalPolyLength;
-
-            }
-
-        }
-
-        private double SumEntityLength_Line(SelectionSet acSelSet)
-        {
-            //get Document and Databse
-            Document acDoc = Application.DocumentManager.MdiActiveDocument;
-            Database acDb = acDoc.Database;
-
-            //define editor variable
-            Editor ed = acDoc.Editor;
-
-            //Set Total Length Variable before beginning the sumation of selected lengths
-            Double totalLineLength = 0;
-
-            using (Transaction acTrans = acDb.TransactionManager.StartTransaction())
-            {
-                try
-                {
-                    //Step through Objects in Selection
-                    foreach (SelectedObject acSelObj in acSelSet)
-                    {
-                        //If there is a valid object, add it's length to the current total
-                        if (acSelObj != null)
-                        {
-                            Entity acEnt = acTrans.GetObject(acSelObj.ObjectId, OpenMode.ForRead) as Entity;
-                            totalLineLength += (acEnt as Line).Length;
-                        }
-                    }
-                }
-                catch
-                {
-                    ed.WriteMessage("Sorry, there was an error.");
-                }
-
-                return totalLineLength;
-
-            }
-
-        }
-
-        private double SumEntityLength_Arc(SelectionSet acSelSet)
-        {
-            //get Document and Databse
-            Document acDoc = Application.DocumentManager.MdiActiveDocument;
-            Database acDb = acDoc.Database;
-
-            //define editor variable
-            Editor ed = acDoc.Editor;
-
-            //Set Total Length Variable before beginning the sumation of selected lengths
-            Double totalArcLength = 0;
-
-            using (Transaction acTrans = acDb.TransactionManager.StartTransaction())
-            {
-                try
-                {
-                    //Step through Objects in Selection
-                    foreach (SelectedObject acSelObj in acSelSet)
-                    {
-                        //If there is a valid object, add it's length to the current total
-                        if (acSelObj != null)
-                        {
-                            Entity acEnt = acTrans.GetObject(acSelObj.ObjectId, OpenMode.ForRead) as Entity;
-                            totalArcLength += (acEnt as Arc).Length;
-                        }
-                    }
-                }
-                catch
-                {
-                    ed.WriteMessage("Sorry, there was an error.");
-                }
-
-                return totalArcLength;
-
-            }
-
-        }
+            }     
+        }        
     }
 }
